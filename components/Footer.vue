bc131eb3707c809482519badd08dddbdb52a606f
cspivack <spivack.craig@gmail.com>
2022-12-25 15:03:01 -0500

    Add link to Mastodon profile

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,24 @@
 <template>
-  <footer>
-    <nuxt-link to="/" class="button">Home</nuxt-link>
-    <button type="button" class="button" @click="toggleForm">Contact</button>
+  <footer id="footer">
+    <div id="footer-left">
+      <nuxt-link to="/" class="button">Home</nuxt-link>
+      <button type="button" class="button" @click="toggleForm">Contact</button>
+    </div>
+    <div id="footer-right">
+      <a
+        href="https://phpc.social/@craig"
+        rel="me"
+        target="_blank"
+        aria-label="Mastodon profile"
+      >
+        <img
+          src="/mastodon.svg"
+          width="20"
+          height="21"
+          alt="Mastodon logo"
+        >
+      </a>
+    </div>
   </footer>
 </template>
 
